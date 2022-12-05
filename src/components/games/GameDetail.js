57b1283b2210daaf838c4cdee91b2c3f2be413ea
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 21:30:38 +0100

    Create GameDetail.js

---
+++ src/components/games/GameDetail.js
@@ -0,0 +1,67 @@
+import { API } from "../../constants/api";
+import axios from "axios";
+import ErrorMessage from "../../common/ErrorMessage";
+import Heading from '../../components/layout/Heading';
+import Loader from "../layout/Loader";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function GameDetail() {
+	const [game, setGame] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+    const http = API;
+
+    let history = useNavigate();
+
+    const { id } = useParams();
+
+    if (!id) {
+        history.push("/");
+    }
+
+	useEffect(
+		function () {
+			async function fetchDataDetail() {
+				try {
+                    const response = await axios.get(http + "/" + id);
+                    console.log(response);
+                if (response.status === 200) {
+                    console.log(response);
+					setGame(response.data);
+                } else {
+                    setError("Failed to upload this particular game page!");
+                }
+                } catch (error) {
+                    setError(error.toString());
+                } finally {
+                    setLoading(false);
+                }
+            }
+			fetchDataDetail();
+		}, 
+	);
+
+	if (loading) {
+		return <Loader />;
+	}
+	if (error) {
+		return <ErrorMessage message={`Error: ${error}`} />;
+	}
+
+	return (
+		<>
+			<section className="card">
+			    <img src={game.images[0].src} alt={game.name} />
+				<article className="card__content">
+				    <Heading content={game.name} />
+			        <p dangerouslySetInnerHTML={{ __html: game.short_description }} />
+					<p><b>Genre: </b>{game.categories[0].name}</p>
+				</article>
+			</section>
+		</>
+	);
+}
+
+export default GameDetail;
