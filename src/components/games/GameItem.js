8752a935e050b4ab2a3265acf737fd8d49474de6
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 21:30:40 +0100

    Create GameItem.js

---
+++ src/components/games/GameItem.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function GameItem({ id, name, images }) {
+	return (
+		<article className="game">
+		    <Link to={`detail/${id}`}>
+				<img src={images[0].src} alt={name} width="100%" />
+				<h2>{name}</h2>
+		    </Link>
+			</article>
+	);
+}
+
+GameItem.propTypes = {
+	id: PropTypes.number.isRequired,
+	name: PropTypes.string.isRequired,
+};
+
+export default GameItem;
