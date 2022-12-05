facfe33ad56430fd8c93389af27d67f3a9c99a44
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 21:30:41 +0100

    Create GameList.js

---
+++ src/components/games/GameList.js
@@ -0,0 +1,58 @@
+import { API } from "../../constants/api";
+import axios from "axios";
+import ErrorMessage from "../../common/ErrorMessage";
+import GameItem from "./GameItem";
+import Heading from "../layout/Heading";
+import Loader from "../layout/Loader";
+import {useState, useEffect} from "react";
+
+const url = API;
+
+function GameList() { 
+    const [games, setGame] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(function () {
+		async function fetchDatas() {
+			try {
+                const response = await axios.get(url);
+                if (response.status === 200) {
+                    setGame(response.data);
+                } else {
+                    setError("A server error occured! Failed to upload games.");
+					console.log(setError);
+                }
+            } catch (error) {
+                setError(error.toString());
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+		}
+		fetchDatas();
+	}, []);
+
+	if (loading) {
+		return <Loader />;
+	}
+
+	if (error) {
+		return <ErrorMessage message={`Error: ${error}`} />;
+	}
+
+
+	return (
+		<>
+		    <Heading content="Game List" />
+		    <section className="games">
+			    {games.map(function (game) {
+				    const { id, name, images } = game;
+				    return <GameItem key={id} id={id} name={name} images={images} />;
+			    })}
+		    </section>
+		</>
+    );
+}
+
+export default GameList; 
