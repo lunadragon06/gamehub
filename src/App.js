e64b1e33b5d13a5badcb41092a20f737baea2732
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 21:30:29 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './sass/style.scss';
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
+import Admin from "./components/pages/Admin"; 
+import ContactForm from "./components/layout/ContactForm";
+import GameList from "./components/games/GameList";
+import GameDetail from "./components/games/GameDetail";
+import LoginForm from "./components/layout/LoginForm";
+import Nav from "./components/layout/Nav";
+
+function App() {
+  return (
+    <AuthProvider>
+    <Router>
+      <Nav />
+      <div className="container">
+      <Routes>
+        <Route path="/" element={<GameList />} />
+        <Route path="/detail/:id" element={<GameDetail />} />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+      </div>
+    </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
