debd3b9496fc72e663100687bc7345d898d94bc3
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-08 21:01:49 +0100

    Create Categories.js

---
+++ src/components/games/Categories.js
@@ -0,0 +1,5 @@
+export default function Categories() {
+	return <div id="categories" className="categories">
+                <button id="all" className="category" name="category" value="15">All genres</button>
+        </div>;
+}
