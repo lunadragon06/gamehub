1dfba3c656bd15dd3323ce6a211b3941fb8dca42
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-08 22:54:59 +0100

    Update Admin.js

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -5,9 +5,7 @@ import { useContext } from 'react';
 export default function AdminPage() {
 
 const [auth] = useContext(AuthContext);
-    // prevent unlogged user to get access to admin page
     if(!auth || auth === null) {
-		// useHistory or useNavigate didn't work, which is why I used this method
         window.location.href = "/login";
     }
 
