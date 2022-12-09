534a5ecd61fa9458f4f6e1eba8bfb70d82a70967
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-09 20:39:21 +0100

    Update Nav.js

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,14 +1,14 @@
 import AuthContext from "../../context/authContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/logo.svg";
 import {  NavLink, useNavigate  } from "react-router-dom";
 import { useContext } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
   const [auth, setAuth] = useContext(AuthContext);
-
+  
 	const history = useNavigate();
 
 	function logout() {
@@ -26,42 +26,50 @@ function Nav() {
 
 	return (
     <nav>
-        <a className="brand" href="/">
-          <img className="logo" src={logo} alt="" /> 
-          <span className="wordmark">GameHub</span>
-        </a>
-        <input type="checkbox" id="nav" />
-        <label className="hamburger" htmlFor="nav">
-          <span />
-          <span />
-          <span />
-        </label>
-        <ul className="menu">
-          <li>
-            <NavLink to="/" onClick="onLinkClick()">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" onClick="onLinkClick()">Contact</NavLink>
-          </li>
-          {auth ? (
-				  <>
-          <li>
-					  <NavLink to="/admin" onClick="onLinkClick()">Admin</NavLink> 
-          </li>
-          <li onClick={logout} className="logbtn">
-            <NavLink to="/" onClick="onLinkClick()" id="userlink">
-              <FontAwesomeIcon icon={faRightFromBracket} style={{ fontSize: '1.4rem', }} />
-            </NavLink>
-          </li>
-				  </>
-			    ) : (
-          <li className="logbtn">
-				    <NavLink to="/login" onClick="onLinkClick()" id="userlink">
-              <FontAwesomeIcon icon={faUser} style={{ fontSize: '1.4rem', }}/>
-            </NavLink>
-          </li>
-			    )}
-        </ul>
+      <a className="brand" href="/">
+        <img className="logo" src={logo} alt="" /> 
+        <span className="wordmark">
+          GameHub
+        </span>
+      </a>
+      <input type="checkbox" id="nav" />
+      <label className="hamburger" htmlFor="nav">
+        <span />
+        <span />
+        <span />
+      </label>
+      <ul className="menu">
+        <li>
+          <NavLink to="/" onClick="onLinkClick()">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" onClick="onLinkClick()">
+            Contact
+          </NavLink>
+        </li>
+        {auth ? (
+				<>
+        <li>
+					<NavLink to="/admin" onClick="onLinkClick()">
+            Admin
+          </NavLink> 
+        </li>
+        <li onClick={logout} className="logbtn">
+          <NavLink to="/" onClick="onLinkClick()" id="userlink">
+            <FontAwesomeIcon icon={faRightFromBracket} style={{ fontSize: '1.4rem', }} />
+          </NavLink>
+        </li>
+				</>
+			  ) : (
+        <li className="logbtn">
+				  <NavLink to="/login" onClick="onLinkClick()" id="userlink">
+            <FontAwesomeIcon icon={faUser} style={{ fontSize: '1.4rem', }}/>
+          </NavLink>
+        </li>
+			  )}
+      </ul>
     </nav>
   );
 }
