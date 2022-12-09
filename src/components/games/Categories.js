d7f2efe28235589d8eb595a8f5bcf106478621f8
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-09 14:32:06 +0100

    Delete Categories.js

--- src/components/games/Categories.js
+++
@@ -1,5 +0,0 @@
-export default function Categories() {
-	return <div id="categories" className="categories">
-                <button id="all" className="category" name="category" value="15">All genres</button>
-        </div>;
-}
